2a6ba923d307b1f0144c75dc435907b106758832
h182037 <sindre.aalhus@gmail.com>
2020-03-09 12:47:22 +0100

    hello world

--- src/main/java/controllers/SpringBootHelloWorld.java
+++ src/main/java/controllers/SpringBootHelloWorld.java
@@ -1,4 +1,20 @@
 package controllers;
 
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
 public class SpringBootHelloWorld {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello, Dat251!";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootHelloWorld.class, args);
+    }
+
 }
